58d3ca952dff05167bea83655cff1d6b62563143
Barry vd. Heuvel <barryvdh@gmail.com>
2015-10-08 22:01:53 +0200

    Update form.php

--- config/form.php
+++ config/form.php
@@ -1,7 +1,21 @@
 <?php
 
 return array(
-
+   
+    /*
+    |--------------------------------------------------------------------------
+    | Form theme
+    |--------------------------------------------------------------------------
+    |
+    | Set the template to render the form. Possible values:
+    | - form_div_layout.html.twig
+    | - form_table_layout.html.twig
+    | - bootstrap_3_layout.html.twig
+    | - bootstrap_3_horizontal_layout.html.twig
+    | - (your own template..)
+    | 
+    | See http://symfony.com/doc/current/cookbook/form/form_customization.html#what-are-form-themes
+    */
     'theme' => 'bootstrap_3_layout.html.twig',
 
 );
