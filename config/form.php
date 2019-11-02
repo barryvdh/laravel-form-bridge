df5797aecec8d8a050e2f26232e7890eeb9ee179
Barry vd. Heuvel <barryvdh@gmail.com>
2019-11-02 14:19:49 +0100

    Set default to Bootstrap 4

--- config/form.php
+++ config/form.php
@@ -12,11 +12,14 @@ return array(
     | - form_table_layout.html.twig
     | - bootstrap_3_layout.html.twig
     | - bootstrap_3_horizontal_layout.html.twig
+    | - bootstrap_4_layout.html.twig
+    | - bootstrap_4_horizontal_layout.html.twig
+    | - foundation_5_layout.html.twig
     | - (your own template..)
     | 
     | See http://symfony.com/doc/current/cookbook/form/form_customization.html#what-are-form-themes
     */
-    'theme' => 'bootstrap_3_layout.html.twig',
+    'theme' => 'bootstrap_4_layout.html.twig',
 
     /*
     |--------------------------------------------------------------------------
