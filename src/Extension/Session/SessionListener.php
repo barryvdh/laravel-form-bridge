5076076206499532d895b1147e0c34fd9d76fe60
Barry vd. Heuvel <barryvdh@gmail.com>
2026-02-23 12:00:13 +0100

    Add return type declaration to getSubscribedEvents

--- src/Extension/Session/SessionListener.php
+++ src/Extension/Session/SessionListener.php
@@ -15,7 +15,7 @@ use Symfony\Component\Form\FormInterface;
 class SessionListener implements EventSubscriberInterface
 {
 
-    public static function getSubscribedEvents()
+    public static function getSubscribedEvents(): array
     {
         return [
             FormEvents::PRE_SET_DATA => 'preSet',
